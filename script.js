fc6217a9c98c9de7f2891edcd3e7bc087df9165f
khantuevao <khantuevao@gmail.com>
2022-06-17 18:16:42 +0200

    Replace logic for divs changing color&array#

--- script.js
+++ script.js
@@ -24,62 +24,25 @@ const gameBoard = (() => {
 
     let color = (() => {
       let result = determinePlayer();
-      return (result === 'X')? '#900' : '#090';
+      return (result === 'X')? 'rgb(153, 0, 0)' : 'rgb(0, 153, 0)';
     })();
 
     let sign = (() => {
       let result = determinePlayer();
       return (result === 'X')? 'X' : 'O';
     })();
-    
+
     return {color, sign}
   }
 
-
-const renderBoard = (() => {
-  const zero = document.getElementById('zero');
-  zero.addEventListener('click', () => {
-  zero.style.backgroundColor = `${currentPlayer().color}`;
-  gameBoard.board[0] = `${currentPlayer().sign}`;
-  });
-  const one = document.getElementById('one');
-  one.addEventListener('click', () => {
-  one.style.backgroundColor = `${currentPlayer().color}`;
-  gameBoard.board[1] = `${currentPlayer().sign}`;
-  });
-  const two = document.getElementById('two');
-  two.addEventListener('click', () => {
-  two.style.backgroundColor = `${currentPlayer().color}`;
-  gameBoard.board[2] = `${currentPlayer().sign}`;
-  });
-  const three = document.getElementById('three');
-  three.addEventListener('click', () => {
-  three.style.backgroundColor = `${currentPlayer().color}`;
-  gameBoard.board[3] = `${currentPlayer().sign}`;
-  });
-  const four = document.getElementById('four');
-  four.addEventListener('click', () => {
-  four.style.backgroundColor = `${currentPlayer().color}`;
-  gameBoard.board[4] = `${currentPlayer().sign}`;
-  });
-  const five = document.getElementById('five');
-  five.addEventListener('click', () => {
-  five.style.backgroundColor = `${currentPlayer().color}`;
-  gameBoard.board[5] = `${currentPlayer().sign}`;
-  });
-  const six = document.getElementById('six');
-  six.addEventListener('click', () => {
-  six.style.backgroundColor = `${currentPlayer().color}`;
-  gameBoard.board[6] = `${currentPlayer().sign}`;
-  });
-  const seven = document.getElementById('seven');
-  seven.addEventListener('click', () => {
-  seven.style.backgroundColor = `${currentPlayer().color}`;
-  gameBoard.board[7] = `${currentPlayer().sign}`;
-  });
-  const eight = document.getElementById('eight');
-  eight.addEventListener('click', () => {
-  eight.style.backgroundColor = `${currentPlayer().color}`;
-  gameBoard.board[8] = `${currentPlayer().sign}`;
-  });
+const areaBoard = (() => {
+  const squares = document.getElementsByClassName('area');
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', () => {
+      if (squares[i].style.backgroundColor === '') {
+        squares[i].style.backgroundColor = `${currentPlayer().color}`;
+        gameBoard.board[i] = `${currentPlayer().sign}`;
+      } else return;
+    })
+  };
 })();
