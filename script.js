83bcef01f27353efb0fcbea07974c69906812d9b
khantuevao <khantuevao@gmail.com>
2022-06-17 19:42:28 +0200

    Add checkResult func

--- script.js
+++ script.js
@@ -5,37 +5,57 @@ const gameBoard = (() => {
   }
   })();
 
-  const currentPlayer = () => {
-    let determinePlayer = () => {
-      let x = 0;
-      let o = 0;
-      gameBoard.board.forEach(e => {
-        if (e === 'X') {
-          return x++;
-        } else if (e === 'O') {
-          return o++;
-        }
-      })
-      let result = (() => {
-        return (x > o)? 'O' : 'X';
-      })();
-      return result;
-    };
-
-    let color = (() => {
-      let result = determinePlayer();
-      return (result === 'X')? 'rgb(153, 0, 0)' : 'rgb(0, 153, 0)';
+const currentPlayer = () => {
+  let determinePlayer = () => {
+    let x = 0;
+    let o = 0;
+    gameBoard.board.forEach(e => {
+      if (e === 'X') {
+        return x++;
+      } else if (e === 'O') {
+        return o++;
+      }
+    })
+    let result = (() => {
+      return (x > o)? 'O' : 'X';
     })();
+    return result;
+  };
 
-    let sign = (() => {
-      let result = determinePlayer();
-      return (result === 'X')? 'X' : 'O';
-    })();
+  let color = (() => {
+    let result = determinePlayer();
+    return (result === 'X')? 'rgb(153, 0, 0)' : 'rgb(0, 153, 0)';
+  })();
 
-    return {color, sign}
+   let sign = (() => {
+    let result = determinePlayer();
+    return (result === 'X')? 'X' : 'O';
+  })();
+
+  return {color, sign}
   }
 
-const areaBoard = (() => {
+
+let player1
+
+let player2
+
+let checkTurn
+
+const checkResult = (() => {
+  const area = gameBoard.board;
+  const result = (() => {
+    return ((area[0] === area[1] && area[0] === area[2]) || 
+            (area[0] === area[3] && area[0] === area[6]) ||
+            (area[0] === area[4] && area[0] === area[8]) ||
+            (area[6] === area[7] && area[6] === area[8]) || 
+            (area[6] === area[4] && area[6] === area[2]) ||
+            (area[8] === area[5] && area[8] === area[2]))? 'win' : 'draw';
+  })();
+  return {result};
+})();
+
+const renderBoard = (() => {
   const squares = document.getElementsByClassName('area');
   for (let i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', () => {
