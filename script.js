ce0ae1b7560a77ff801a48cf78f63788a03b77f9
khantuevao <khantuevao@gmail.com>
2022-06-17 09:43:48 +0200

    Set up html and css gameboard

---
+++ script.js
@@ -0,0 +1,20 @@
+const gameBoard = (() => {
+  const board=  [0, 1, 2,
+                 3, 4, 5,
+                 6, 7, 8];
+  return {
+    board
+  }
+  })();
+
+const player = {
+  
+}
+
+const gameFlow = {
+
+}
+
+const renderBoard = () => {
+
+}
