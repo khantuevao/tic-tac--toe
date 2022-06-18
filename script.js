73688b2b5bd241745f461afff378a8c35ad62416
khantuevao <khantuevao@gmail.com>
2022-06-18 10:19:35 +0200

    Add button to enter players names and clear score

--- script.js
+++ script.js
@@ -5,54 +5,57 @@ const gameBoard = (() => {
   }
   })();
 
-const currentPlayer = () => {
-  let determinePlayer = () => {
-    let x = 0;
-    let o = 0;
-    gameBoard.board.forEach(e => {
-      if (e === 'X') {
-        return x++;
-      } else if (e === 'O') {
-        return o++;
-      }
-    })
-    let result = (() => {
-      return (x > o)? 'O' : 'X';
-    })();
-    return result;
-  };
+const newPlayer = () => {
+  const name = prompt(`enter player name`, '');
+  return name;
+}
 
-  let color = (() => {
-    let result = determinePlayer();
-    return (result === 'X')? 'rgb(153, 0, 0)' : 'rgb(0, 153, 0)';
-  })();
 
-   let sign = (() => {
-    let result = determinePlayer();
-    return (result === 'X')? 'X' : 'O';
-  })();
 
-  return {color, sign}
+
+const gameFlow = (() => {
+  const clear = () => {
+    resultContainer.textContent = '';
+    gameBoard.board = [];
+    const squares = document.getElementsByClassName('area');
+    for (let i = 0; i < squares.length; i++) {
+      squares[i].style.backgroundColor = '';
+    };      
   }
 
-
-let player1
-
-let player2
-
-let checkTurn
-
-const checkResult = () => {
-  const checkIsOver = () => {
-    const result = (() => {
-      let count = 0;
-      for (let i = 0; i < 9; i++) {
-        if (gameBoard.board[i] === undefined) count++;
-      }
-      return (count === 0)? true : false;
+  let firstPlayer = 'playerOne';
+  let secondPlayer = 'playerTwo';
+  const newPlayers = document.querySelector('.new-players');
+  newPlayers.addEventListener('click', () => {
+    firstPlayer = newPlayer(one);
+    secondPlayer = newPlayer(two);
+    const updateNames = (() => {
+      const nameOne = document.getElementById('name-one');
+      nameOne.textContent = `${firstPlayer} (red)`;
+      let scoreOne = document.getElementById('score-one');
+      scoreOne.textContent = '0';
+      const nameTwo = document.getElementById('name-two');
+      nameTwo.textContent = `${secondPlayer} (green)`;
+      let scoreTwo = document.getElementById('score-two');
+      scoreTwo.textContent = '0';
+      clear();
     })();
-    return result;
-  };
+  });
+
+  const resultContainer = document.getElementById('result-container');
+
+  const checkResult = () => {
+    const checkIsOver = () => {
+      const result = (() => {
+        let count = 0;
+        for (let i = 0; i < 9; i++) {
+          if (gameBoard.board[i] === undefined) count++;
+        }
+        return (count === 0)? true : false;
+      })();
+      return result;
+    };
+
   const area = gameBoard.board;
   const result = (() => {
     if ((area[0] === 'X' && area[1] === 'X' && area[2] === 'X') || 
@@ -78,43 +81,65 @@ const checkResult = () => {
         } else {
           return;
         }
-  })();
-  return {result};
-};
-
-const resultContainer = document.getElementById('result-container');
-
-const renderBoard = (() => {
-  const squares = document.getElementsByClassName('area');
-  for (let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', () => {
-      if (!(resultContainer.textContent === '')) {
-        resultContainer.textContent = '';
-        gameBoard.board = [];
-        for (let i = 0; i < squares.length; i++) {
-          squares[i].style.backgroundColor = '';
-        };
-      };
-      if (squares[i].style.backgroundColor === '') {
-        squares[i].style.backgroundColor = `${currentPlayer().color}`;
-        gameBoard.board[i] = `${currentPlayer().sign}`;
-        showResult();
-      } else return;
-    })
+    })();
+    return {result};
   };
-})();
 
-const showResult = () => {
-  let final = checkResult();
-  if (final.result === 'X') {
-    let scoreOne = document.getElementById('score-one');
-    scoreOne.textContent = Number(scoreOne.textContent) + 1;
-    resultContainer.textContent = 'playerOne wins';
-  } else if (final.result === 'O') {
-    let scoreTwo = document.getElementById('score-two');
-    scoreTwo.textContent = Number(scoreTwo.textContent) + 1;
-    resultContainer.textContent = 'playerTwo wins';
-  } else if (final.result === 'draw') {
-    resultContainer.textContent = 'it\'s a draw';
+  const showResult = () => {
+    let final = checkResult();
+    if (final.result === 'X') {
+      let scoreOne = document.getElementById('score-one');
+      scoreOne.textContent = Number(scoreOne.textContent) + 1;
+      resultContainer.textContent = `${firstPlayer} wins`;
+    } else if (final.result === 'O') {
+      let scoreTwo = document.getElementById('score-two');
+      scoreTwo.textContent = Number(scoreTwo.textContent) + 1;
+      resultContainer.textContent = `${secondPlayer} wins`;
+    } else if (final.result === 'draw') {
+      resultContainer.textContent = 'it\'s a draw';
+    }
   }
-}
+
+  const checkTurn = () => {
+    let determinePlayer = () => {
+      let x = 0;
+      let o = 0;
+      gameBoard.board.forEach(e => {
+        if (e === 'X') {
+          return x++;
+        } else if (e === 'O') {
+          return o++;
+        }
+      })
+      let result = (() => {
+        return (x > o)? 'O' : 'X';
+      })();
+      return result;
+    };
+    let color = (() => {
+      let result = determinePlayer();
+      return (result === 'X')? 'rgb(153, 0, 0)' : 'rgb(0, 153, 0)';
+    })();
+     let sign = (() => {
+      let result = determinePlayer();
+      return (result === 'X')? 'X' : 'O';
+    })();
+    return {color, sign}
+  }
+
+  const renderBoard = (() => {
+    const squares = document.getElementsByClassName('area');
+    for (let i = 0; i < squares.length; i++) {
+      squares[i].addEventListener('click', () => {
+        if (!(resultContainer.textContent === '')) {
+          clear();
+        };
+        if (squares[i].style.backgroundColor === '') {
+          squares[i].style.backgroundColor = `${checkTurn().color}`;
+          gameBoard.board[i] = `${checkTurn().sign}`;
+          showResult();
+        } else return;
+      })
+    };
+  })();
+})();
